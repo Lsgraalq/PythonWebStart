27c8cc2683f8b1c6941f53239db0fbcf649578bc
lsgraalq <ilya.merinec@gmail.com>
2023-09-28 19:09:11 +0300

    added posts

--- board/views.py
+++ board/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+
+from .models import BoardMessage 
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("АлеДаДа")
+    content = 'Список оголошень\n\n'
+    for post in BoardMessage.objects.order_by('-published'):
+        content += post.title + '\n' + post.content + '\n\n'
+
+    return HttpResponse(content, content_type = 'text/plain; charset=utf-8')
