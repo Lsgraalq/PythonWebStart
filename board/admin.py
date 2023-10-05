8491a2dd5c1673b308fcf7620d4592621d00f3aa
lsgraalq <ilya.merinec@gmail.com>
2023-10-05 18:45:28 +0300

    add board messageAdmin

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import BoardMessage 
+
+
+class BoardMessageAdmin( admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(BoardMessage, BoardMessageAdmin)
--- board/views.py
+++ board/views.py
@@ -4,6 +4,6 @@ from .models import BoardMessage
 
 # Create your views here.
 def index(request):
-    posts = BoardMessage.objects.order_by('-published')
+    posts = BoardMessage.objects.all()
     context =  {'posts' : posts}
     return render(request, 'board/index.html', context ) 
